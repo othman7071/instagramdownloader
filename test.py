6e791d06ce37d520bda8a30205d2b1aa17fb8ab1
othman <othmanalhaialy2004@gmail.com>
2022-04-20 19:15:18 +0300

    remove useless images

--- test.py
+++
@@ -1,6 +0,0 @@
-
-url = 'https://www.instagram.com/tv/CImdRHRhs2f/'
-instagram_url = 'https://www.instagram.com'
-
-if not ( url.startswith(instagram_url + '/p') or url.startswith(instagram_url + '/tv') ):
-            print('Invalid url')
